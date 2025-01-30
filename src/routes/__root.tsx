ba7abf4716d65127012e02a044715bdeb2386d92
marcopetry <marcomattospetry@gmail.com>
2025-01-30 16:34:32 -0300

    feat: add routes to the project

---
+++ src/routes/__root.tsx
@@ -0,0 +1,11 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { Navbar } from "../components/navbar";
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  ),
+});

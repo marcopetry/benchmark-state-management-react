a99ebc7b306dde682f6d9c8dca5713812c588d88
marcopetry <marcomattospetry@gmail.com>
2025-01-30 16:35:33 -0300

    feat: add navbar component

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,22 @@
+import { Link } from "@tanstack/react-router";
+
+import styles from "./navbar.module.css";
+
+export const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li>
+          <Link to="/" title="Página inicial">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/context-api" title="Sobre nós">
+            Context Api
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};

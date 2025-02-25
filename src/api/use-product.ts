94a2cd4a26818f6deebb862082be816505efad71
marcopetry <marcomattospetry@gmail.com>
2025-02-24 23:37:55 -0300

    feat: add mock api to products

---
+++ src/api/use-product.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import { fetchMockData } from "./api";
+import { Product } from "../types/product.types";
+
+export const useProducts = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  useEffect(() => {
+    const fetch = async () => {
+      const data = await fetchMockData();
+      setProducts(data);
+    };
+
+    fetch();
+  }, []);
+
+  return { products };
+};

77e3eeb02d906d1929183fb81214a7bffc5530b5
marcopetry <marcomattospetry@gmail.com>
2025-02-27 13:19:56 -0300

    refactor: pass components to card product

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import { useProducts } from "../api/use-product";
 import { CardProduct } from "../components/card-product";
+import { CardProductDescription } from "../components/card-product/card-product-description";
+import { CardProductImage } from "../components/card-product/card-product-image";
+import { CardProductPrice } from "../components/card-product/card-product-price";
+import { CardProductRating } from "../components/card-product/card-product-rating";
+import { CardProductTitle } from "../components/card-product/card-product-title";
 import { Layout } from "../components/layout";
 
 export const ProductList = () => {
@@ -9,9 +14,17 @@ export const ProductList = () => {
       {products.map((product) => (
         <CardProduct
           key={product.id}
-          product={product}
-          onClick={() => {}}
-          onClickAction={() => {}}
+          onClick={() => {
+            console.log(product);
+          }}
+          onClickAction={() => {
+            console.log(product);
+          }}
+          productImage={<CardProductImage product={product} />}
+          productTitle={<CardProductTitle product={product} />}
+          productDescription={<CardProductDescription product={product} />}
+          productPrice={<CardProductPrice product={product} />}
+          productRating={<CardProductRating product={product} />}
         />
       ))}
     </Layout>

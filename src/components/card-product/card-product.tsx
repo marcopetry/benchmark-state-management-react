e1f5a8295adebd25b17b77e659102907c6064f7a
marcopetry <marcomattospetry@gmail.com>
2025-02-27 13:19:33 -0300

    refactor: use composition to components

--- src/components/card-product/card-product.tsx
+++ src/components/card-product/card-product.tsx
@@ -4,41 +4,36 @@ import styles from "./card-product.module.css";
 import { Button } from "../button";
 
 type CardProductProps = {
-  product: Product;
-  onClick: (product: Product) => void;
-  onClickAction: (product: Product) => void;
+  productImage: ReactNode;
+  productTitle: ReactNode;
+  productDescription: ReactNode;
+  productPrice: ReactNode;
+  productRating: ReactNode;
+  onClick: VoidFunction;
+  onClickAction: VoidFunction;
 };
 
-function renderStars(rating: number) {
-  const fullStars = Math.floor(rating); // Estrelas cheias
-  const halfStar = rating % 1 >= 0.5 ? 1 : 0; // Meia estrela se houver decimal >= 0.5
-  const emptyStars = 5 - fullStars - halfStar; // O restante são estrelas vazias
-
-  return "★".repeat(fullStars) + "⯪".repeat(halfStar) + "☆".repeat(emptyStars);
-}
-
 export const CardProduct = ({
-  product,
   onClick,
   onClickAction,
+  productDescription,
+  productImage,
+  productPrice,
+  productRating,
+  productTitle,
 }: CardProductProps) => {
-  const { description, image, price, title, rating } = product;
-
   return (
-    <div className={styles.card} onClick={() => onClick(product)}>
-      <img className={styles.image} src={image} alt={title} />
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.description}>{description}</p>
-      <p className={styles.price}>R$ {price}</p>
+    <div className={styles.card} onClick={() => onClick()}>
+      {productImage}
+      {productTitle}
+      {productDescription}
+      {productPrice}
 
-      <Button onClick={() => onClickAction(product)} variant="success">
+      <Button onClick={() => onClickAction()} variant="success">
         Adicionar ao Carrinho
       </Button>
 
-      <div className={styles.rating}>
-        <div className={styles.stars}>{renderStars(rating.rate)}</div>
-        <span className={styles.count}>({rating.count} avaliações)</span>
-      </div>
+      {productRating}
     </div>
   );
 };

2b68cf751eb49873f19355b51e3a111ef11c56f0
marcopetry <marcomattospetry@gmail.com>
2025-02-27 13:18:10 -0300

    feat: isolate components

---
+++ src/components/card-product/card-product-price.tsx
@@ -0,0 +1,6 @@
+import { Product } from "../../types/product.types";
+import styles from "./card-product.module.css";
+
+export const CardProductPrice = ({ product }: { product: Product }) => {
+  return <p className={styles.price}>R$ {product.price}</p>;
+};

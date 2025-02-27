c5fd081da8d4b8045d9e50677879257de3defba4
marcopetry <marcomattospetry@gmail.com>
2025-02-27 13:42:44 -0300

    feat: add use cases to example in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,27 +5,31 @@ import { CardProductImage } from "../components/card-product/card-product-image"
 import { CardProductPrice } from "../components/card-product/card-product-price";
 import { CardProductRating } from "../components/card-product/card-product-rating";
 import { CardProductTitle } from "../components/card-product/card-product-title";
+import {
+  CardProductCarShop,
+  CardProductDefault,
+  CardProductWithDiscount,
+} from "../components/card-product/use-cases";
 import { Layout } from "../components/layout";
 
 export const ProductList = () => {
   const { products } = useProducts();
+
+  const productsWithDiscount = products.slice(0, 4);
+  const productsCardShop = products.slice(3, 5);
+
   return (
     <Layout>
+      {productsWithDiscount.map((product) => (
+        <CardProductWithDiscount key={product.id} product={product} />
+      ))}
+
       {products.map((product) => (
-        <CardProduct
-          key={product.id}
-          onClick={() => {
-            console.log(product);
-          }}
-          onClickAction={() => {
-            console.log(product);
-          }}
-          productImage={<CardProductImage product={product} />}
-          productTitle={<CardProductTitle product={product} />}
-          productDescription={<CardProductDescription product={product} />}
-          productPrice={<CardProductPrice product={product} />}
-          productRating={<CardProductRating product={product} />}
-        />
+        <CardProductDefault key={product.id} product={product} />
+      ))}
+
+      {productsCardShop.map((product) => (
+        <CardProductCarShop key={product.id} product={product} />
       ))}
     </Layout>
   );

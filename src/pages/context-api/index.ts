3c2e228177442083ec53d3def919a653b60271b6
marco.petry <marcomattospetry@gmail.com>
2025-06-07 23:43:02 -0300

    feat: export pages to context api

--- src/pages/context-api/index.ts
+++ src/pages/context-api/index.ts
@@ -1,1 +1,4 @@
-export * from "./context-api";
+export * from "./context-api-page-products";
+export * from "./context-api-page-product-details";
+export * from "./context-api-page-cart";
+export * from "./context-api-page-checkout";

8705d9885a96f558d7520b82bfd18c03d83f87e8
marco.petry <marcomattospetry@gmail.com>
2025-06-07 22:09:31 -0300

    feat: use 2 fixed on price

--- src/components/card-product/card-product-price.tsx
+++ src/components/card-product/card-product-price.tsx
@@ -2,5 +2,5 @@ import { Product } from "@/types/product.types";
 import styles from "./card-product.module.css";
 
 export const CardProductPrice = ({ product }: { product: Product }) => {
-  return <p className={styles.price}>R$ {product.price}</p>;
+  return <p className={styles.price}>R$ {product.price.toFixed(2)}</p>;
 };

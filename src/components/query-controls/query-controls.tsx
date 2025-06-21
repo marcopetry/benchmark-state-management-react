a3243397bddf8e752bedb56c3cc02f56a904057b
marco.petry <marcomattospetry@gmail.com>
2025-06-21 15:51:47 -0300

    chore: remove robot param

--- src/components/query-controls/query-controls.tsx
+++ src/components/query-controls/query-controls.tsx
@@ -6,7 +6,6 @@ export const QueryControls = () => {
   const navigate = useNavigate();
   const search = useSearch({ strict: false });
 
-  const [robot, setRobot] = useState(search.robot === "true");
   const [items, setItems] = useState(Number(search.items) || 100);
 
   useEffect(() => {
@@ -14,12 +13,11 @@ export const QueryControls = () => {
       search: (prev) =>
         ({
           ...prev,
-          robot: String(robot),
           items: String(items),
         } as never),
       replace: true,
     });
-  }, [robot, items, navigate]);
+  }, [items, navigate]);
 
   return (
     <div className={styles.container}>
